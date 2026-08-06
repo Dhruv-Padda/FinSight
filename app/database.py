baecbf1a5460f6e7d99b0cd0ad25c88e959ef6a2
Dhruv Padmanabha <dhruvpadda17@gmail.com>
2026-08-06 15:56:59 +0530

    Implement Expense creation endpoint

--- app/database.py
+++ app/database.py
@@ -1,8 +1,20 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from app.models import Base
+from sqlalchemy.orm import sessionmaker
+
 
 DATABASE_URL = "postgresql://dhruv:password@localhost:5432/expense_tracker"
 
 engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit = False)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 Base.metadata.create_all(bind=engine)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from app.database import get_db
 from app.schemas import ExpenseCreate
+from app.models import Expense
+
 
 app = FastAPI()
 
 @app.post("/expenses")
-def create_expense(expense: ExpenseCreate):
-    return expense
+def create_expense(
+    expense: ExpenseCreate,
+    db: Session = Depends(get_db)
+):
+    db_expense = Expense(user_id = 1,
+    amount  = expense.amount,
+    category = expense.category,
+    description = expense.description,
+    date = expense.date)
+
+    db.add(db_expense)
+    db.commit()
+    db.refresh(db_expense)
+
+    return db_expense
